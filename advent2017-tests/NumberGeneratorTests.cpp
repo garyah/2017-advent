0d13e913c0b259b0c0d096a89fdc1f8dd724fcd6
ghilde <ghilde@microsoft.com>
2017-12-16 11:20:31 -0800

    New class for day 15 problem for generating the numbers needed from each generator.

---
+++ advent2017-tests/NumberGeneratorTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../advent2017/NumberGenerator.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Advent2017;
+
+namespace advent2017tests
+{
+    TEST_CLASS(NumberGeneratorTests)
+    {
+    public:
+
+        TEST_METHOD(SomeTest)
+        {
+            //Assert::AreEqual(3u, CaptchaDigitSummer::sumRepeating("1122"));
+        }
+    };
+}
---
+++ advent2017/NumberGenerator.hpp
@@ -0,0 +1,8 @@
+namespace Advent2017
+{
+    class NumberGenerator
+    {
+    public:
+    private:
+    };
+}
