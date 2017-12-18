b4d6712a3e24307956b86f03538ce7d6229bdf01
ghilde <ghilde@microsoft.com>
2017-12-17 22:34:16 -0800

    Get day 18 drivers ready.

--- 18/18a.cpp
+++ 18/18a.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 #include "../common/InputReader.hpp"
-#include "../advent2016/SafeTileCounter.hpp"
+//#include "../advent2017/SafeTileCounter.hpp"
 
 using namespace Common;
-using namespace Advent2016;
+//using namespace Advent2017;
 
 int main()
 {
     char *line = NULL;
     while ((line = InputReader::ReadLine()) != NULL)
     {
-        SafeTileCounter counter;
-        counter.countTiles(line, 40);
-        (void)printf("%u\n", counter.getNumberSafeTiles());
+        //SafeTileCounter counter;
+        //counter.countTiles(line, 40);
+        //(void)printf("%u\n", counter.getNumberSafeTiles());
     }
     return 0;
 }
