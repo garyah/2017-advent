63f99111bca704dea87f978748505d3d50e6cfcd
ghilde <ghilde@microsoft.com>
2017-12-17 17:52:26 -0800

    Add ability for generator to only output numbers in sequence divisible by certain factor.

--- advent2017/NumberGenerator.hpp
+++ advent2017/NumberGenerator.hpp
@@ -3,15 +3,18 @@ namespace Advent2017
     class NumberGenerator
     {
     public:
-        NumberGenerator(unsigned initialNumber, unsigned factor) :
-            m_number(initialNumber), m_factor(factor)
+        NumberGenerator(unsigned initialNumber, unsigned factor, unsigned divisibleBy = 1u) :
+            m_number(initialNumber), m_factor(factor), m_divisibleBy(divisibleBy)
         {
         }
 
         unsigned getNextNumber()
         {
-            uint64_t product = (uint64_t)m_number * m_factor;
-            m_number = calculateMaxSigned32BitRemainder(product);
+            do
+            {
+                uint64_t product = (uint64_t)m_number * m_factor;
+                m_number = calculateMaxSigned32BitRemainder(product);
+            } while ((m_number % m_divisibleBy) != 0);
             return m_number;
         }
 
@@ -23,5 +26,6 @@ namespace Advent2017
 
         unsigned m_number;
         unsigned m_factor;
+        unsigned m_divisibleBy;
     };
 }
