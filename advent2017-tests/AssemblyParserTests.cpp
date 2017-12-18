de64ffa5b58a11ce6d885b59c106297a787f9824
ghilde <ghilde@microsoft.com>
2017-12-17 22:51:00 -0800

    Skeleton of parser with unit test for sample program in 1st part problem for day 18.  Test naturally fails.

--- advent2017-tests/AssemblyParserTests.cpp
+++ advent2017-tests/AssemblyParserTests.cpp
@@ -11,10 +11,21 @@ namespace advent2017tests
     TEST_CLASS(AssemblyParserTests)
     {
     public:
-        TEST_METHOD(SomeTest)
+        TEST_METHOD(SampleProgramReturnsCorrectLastRecoveryValue)
         {
-            //AssemblyParser someObject();
-            //Assert::AreEqual(expected, someObject.someMethod());
+            AssemblyParser parser;
+            parser.parseInstruction("set a 1");
+            parser.parseInstruction("add a 2");
+            parser.parseInstruction("mul a a");
+            parser.parseInstruction("mod a 5");
+            parser.parseInstruction("snd a");
+            parser.parseInstruction("set a 0");
+            parser.parseInstruction("rcv a");
+            parser.parseInstruction("jgz a -1");
+            parser.parseInstruction("set a 1");
+            parser.parseInstruction("jgz a -2");
+            parser.executeProgram();
+            Assert::AreEqual(4u, parser.getLastRecoveryValue());
         }
     };
 }
--- advent2017/AssemblyParser.hpp
+++ advent2017/AssemblyParser.hpp
@@ -1,8 +1,28 @@
+#include <string>
+#include <vector>
+
 namespace Advent2017
 {
     class AssemblyParser
     {
     public:
+        AssemblyParser() :
+            m_program() {}
+
+        void parseInstruction(const char *program)
+        {
+        }
+
+        void executeProgram()
+        {
+        }
+
+        unsigned getLastRecoveryValue()
+        {
+            return 0;
+        }
+
     private:
+        std::vector<std::string> m_program;
     };
 }
