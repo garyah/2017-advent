07fab8f78747b0ac4b7b6eecbf9f02fe4c0ef840
ghilde <ghilde@microsoft.com>
2017-12-18 09:50:42 -0800

    Complete driver for 1st part day 18.

--- 18/18a.cpp
+++ 18/18a.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 #include "../common/InputReader.hpp"
-//#include "../advent2017/SafeTileCounter.hpp"
+#include "../advent2017/AssemblyParser.hpp"
 
 using namespace Common;
-//using namespace Advent2017;
+using namespace Advent2017;
 
 int main()
 {
+    AssemblyParser parser;
     char *line = NULL;
     while ((line = InputReader::ReadLine()) != NULL)
     {
-        //SafeTileCounter counter;
-        //counter.countTiles(line, 40);
-        //(void)printf("%u\n", counter.getNumberSafeTiles());
+        parser.parseInstruction(line);
     }
+    parser.executeProgram();
+    (void)printf("%u\n", parser.getFirstRecoveryValue());
     return 0;
 }
