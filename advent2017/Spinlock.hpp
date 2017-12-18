8ad21718e80a6a268a186ce0161d4fac7b3dfd15
ghilde <ghilde@microsoft.com>
2017-12-17 19:54:42 -0800

    r

--- advent2017/Spinlock.hpp
+++ advent2017/Spinlock.hpp
@@ -11,21 +11,34 @@ namespace Advent2017
             buffer.push_back(0);
             size_t currentPosition = 0;
             for (auto valueToInsert = 1u; valueToInsert <= 2017; ++valueToInsert)
-            {
-                currentPosition += stepSize;
-                currentPosition %= buffer.size();
-                std::list<unsigned>::iterator iterator = buffer.begin();
-                for (size_t count = 0;
-                    count < currentPosition && iterator != buffer.end();
-                    ++count, ++iterator);
-                (void)buffer.insert(iterator, valueToInsert);
-                ++currentPosition;
-            }
-            for (size_t count = 0; count < currentPosition; ++count)
-                (void)buffer.pop_front();
-            return buffer.front();
+                setCurrentPositionAndInsertValue(buffer, stepSize, valueToInsert, currentPosition);
+            return valueAtPosition(buffer, currentPosition);
         }
 
     private:
+
+        static void setCurrentPositionAndInsertValue(std::list<unsigned>& buffer, size_t stepSize,
+                                                     unsigned valueToInsert, size_t& currentPosition)
+        {
+            currentPosition = (currentPosition + stepSize) % buffer.size();
+            insertAtPosition(buffer, valueToInsert, currentPosition);
+            ++currentPosition;
+        }
+
+        static void insertAtPosition(std::list<unsigned>& buffer, unsigned valueToInsert, size_t position)
+        {
+            std::list<unsigned>::iterator iterator = buffer.begin();
+            for (size_t count = 0;
+                count < position && iterator != buffer.end();
+                ++count, ++iterator);
+            (void)buffer.insert(iterator, valueToInsert);
+        }
+
+        static unsigned valueAtPosition(std::list<unsigned> buffer, size_t position)
+        {
+            for (size_t count = 0; count < position; ++count)
+                (void)buffer.pop_front();
+            return buffer.front();
+        }
     };
 }
