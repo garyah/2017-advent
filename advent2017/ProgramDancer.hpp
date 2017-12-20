204e90e5e938602c629ca26444408f373f4cf302
Gary Hildebrand <ghilde@microsoft.com>
2017-12-19 21:59:33 -0800

    Complete spin enough to pass test based on sample.

--- advent2017/ProgramDancer.hpp
+++ advent2017/ProgramDancer.hpp
@@ -10,8 +10,8 @@ namespace Advent2017
         {
             char result[26 + 1] = { 0 };
             char programName = 'a';
-            for (size_t i = 0; i < numberOfPrograms && i < _countof(result) - 1; ++i)
-                result[i] = programName++;
+            for (size_t idx = 0; idx < numberOfPrograms && idx < _countof(result) - 1; ++idx)
+                result[idx] = programName++;
 
             char mutableMoveList[80] = { 0 };
             (void)strncpy_s(mutableMoveList, moveList, _countof(mutableMoveList));
@@ -19,6 +19,18 @@ namespace Advent2017
             auto moveToken = strtok_s(mutableMoveList, " ,", &context);
             while (moveToken != nullptr)
             {
+                if (moveToken[0] == 's') // Spin
+                {
+                    char newResult[26 + 1] = { 0 };
+                    auto numberOfProgramsToMoveToFront = (size_t)atoi(moveToken + 1);
+                    auto indexOfFirst = strlen(result) - numberOfProgramsToMoveToFront;
+                    size_t dstIdx = 0;
+                    for (size_t srcIdx = indexOfFirst; srcIdx < strlen(result); ++srcIdx)
+                        newResult[dstIdx++] = result[srcIdx];
+                    for (size_t srcIdx = 0; srcIdx < indexOfFirst; ++srcIdx)
+                        newResult[dstIdx++] = result[srcIdx];
+                    (void)strcpy_s(result, _countof(result) - 1, newResult);
+                }
                 moveToken = strtok_s(nullptr, " ,", &context);
             }
 
