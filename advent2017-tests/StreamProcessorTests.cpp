a278dbf671e3ce25b14e396e03df1fa82c1978bf
Gary Hildebrand <ghilde@microsoft.com>
2017-12-20 23:21:50 -0800

    New code and test modules for day 9, adding to project/solution.

---
+++ advent2017-tests/StreamProcessorTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../advent2017/StreamProcessor.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Advent2017;
+
+namespace advent2017tests
+{
+    TEST_CLASS(StreamProcessorTests)
+    {
+    public:
+        TEST_METHOD(SomeTest)
+        {
+            //StreamProcessor someObject();
+            //Assert::AreEqual(expected, someObject.someMethod());
+        }
+    };
+}
---
+++ advent2017/StreamProcessor.hpp
@@ -0,0 +1,8 @@
+namespace Advent2017
+{
+    class StreamProcessor
+    {
+    public:
+    private:
+    };
+}
