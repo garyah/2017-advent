fb85ff52fe5ff16f0819426cfd0ee5e67c28649a
Gary Hildebrand <ghilde@microsoft.com>
2017-12-27 01:56:22 -0800

    Updated driver, (correct) problem output for day 9 part 1.  Updated scoring.

--- 09/09a.cpp
+++ 09/09a.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 #include "../common/InputReader.hpp"
-#include "../advent2016/ErrorCorrector.hpp"
+#include "../advent2017/StreamProcessor.hpp"
 
 using namespace Common;
-using namespace Advent2016;
+using namespace Advent2017;
+
+unsigned StreamProcessor::TotalGroupScore = 0;
 
 int main()
 {
-    ErrorCorrector corrector;
     char *line = NULL;
-	while ((line = InputReader::ReadLine()) != NULL)
-	{
-        corrector.addMessage(line);
-	}
-    corrector.doForwardCorrection();
-    (void)printf("%s\n", corrector.getCorrected());
+    while ((line = InputReader::ReadLine()) != NULL)
+    {
+        StreamProcessor::TotalGroupScore = 0;
+        auto numGroups = 0u;
+        auto groupScore = 0u;
+        (void)StreamProcessor::EatGroup(line, numGroups, &groupScore);
+    }
+    (void)printf("%u\n", StreamProcessor::TotalGroupScore);
     return 0;
 }
